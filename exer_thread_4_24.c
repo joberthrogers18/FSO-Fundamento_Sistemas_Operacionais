e3dc935e33bedb16d1bcb62b5802b03913ae23bb
Joberth Rogers <joberth.rogers18@gmail.com>
2018-09-26 16:58:28 -0300

    starting exercise FSO thread

---
+++ exer_thread_4_24.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <pthread.h>
+
+void *primos(void *argumentos){
+  int id = *(int *)argumentos;
+  printf("%d\n", id);
+
+  pthread_exit(NULL);
+}
+
+int main(){
+
+  pthread_t thread_id;
+  int flag, status;
+
+  int number = 0;
+
+  printf("Entre com um numero: ");
+  scanf("%d", &number);
+
+  flag = pthread_create(&thread_id, NULL, primos, &number);
+  if(flag != 0) printf("Erro na criação da nova thread");
+
+  status = pthread_join(thread_id, NULL);
+
+  return 0;
+}
