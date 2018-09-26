e39b6242c9bf5b4128378a920a7ce9b5ba53a6dd
Joberth Rogers <joberth.rogers18@gmail.com>
2018-09-26 17:58:15 -0300

    adding function to print prime numbers with thread

--- exer_thread_4_24.c
+++ exer_thread_4_24.c
@@ -1,9 +1,20 @@
 #include <stdio.h>
 #include <pthread.h>
 
-void *primos(void *argumentos){
-  int id = *(int *)argumentos;
-  printf("%d\n", id);
+void *print_prime_numbers(void *argumentos){
+  int number = *(int *)argumentos;
+  int i, j ,times = 0;
+  for(i=0; i < number; i++){
+    times = 0;
+    for(j= i; j > 0; j--){
+      if(i % j == 0 ){
+        times += 1;
+      }
+    }
+    if(times <= 2 ){
+      printf("%d ", i);
+    }
+  }
 
   pthread_exit(NULL);
 }
@@ -18,7 +29,7 @@ int main(){
   printf("Entre com um numero: ");
   scanf("%d", &number);
 
-  flag = pthread_create(&thread_id, NULL, primos, &number);
+  flag = pthread_create(&thread_id, NULL, print_prime_numbers, &number);
   if(flag != 0) printf("Erro na criação da nova thread");
 
   status = pthread_join(thread_id, NULL);
