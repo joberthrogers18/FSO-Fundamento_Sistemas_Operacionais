d6c8552ee9ecf212560cafaf065a3ed1f3350f59
Joberth Rogers <joberth.rogers18@gmail.com>
2018-11-04 17:39:13 -0200

    try to implement semaphore

--- trab1/students.c
+++ trab1/students.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
+#include <semaphore.h>
+#include <unistd.h>
+
+sem_t mutexSem;
+
+void* thread (void* arg){
+    //wait
+    sem_wait(&mutexSem);
+    printf("Entrou\n");
+
+    sleep(1);
+
+    printf("Saindo\n");
+    sem_post(&mutexSem);
+}
 
 void* hello (void* args){
     printf("Eu sou um estudante\n");
@@ -18,19 +33,36 @@ int number_students(){
 }
 
 int main(){
+
+    sem_init(&mutexSem, 0, 1);
+
     int numberStudents;
     srand(time(NULL));
 
     numberStudents =  number_students();
 
     pthread_t thread_students_id[numberStudents];
+    pthread_t tAE_id;
+    pthread_t tAE_id2;
 
-    for(int i = 0; i < numberStudents; i++)
+    pthread_create(&tAE_id, NULL, thread, NULL);
+
+    /*for(int i = 0; i < numberStudents; i++)
         pthread_create(&thread_students_id[i], NULL, hello, NULL);
 
     for(int i = 0 ; i < numberStudents; i++)
         pthread_join(thread_students_id[i], NULL);
+    */
 
+   sleep(2);
+
+   pthread_create(&tAE_id2, NULL, thread, NULL);
+
+   pthread_join(tAE_id, NULL);
+   pthread_join(tAE_id2, NULL);
+
+
+   sem_destroy(&mutexSem);
 
     return 0;
 }
