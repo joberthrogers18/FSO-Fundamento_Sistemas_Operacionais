7756c5a7b443296cbc2342c69441cafdf0d6aa43
Joberth Rogers <joberth.rogers18@gmail.com>
2018-11-25 21:48:00 -0200

    read the address and define the number page and offset

--- trab2/mmv.c
+++ trab2/mmv.c
@@ -1,25 +1,54 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-int read_adress(){
+#define NUMBER_ADRESSES_LOGICAL 10000
+#define MASKOFFSET 15
+#define MASKPAGENUMBER 240
+
+struct adress_logical{
+    int number_page;
+    int offset;
+};
+
+int adresses_logical_numbers[NUMBER_ADRESSES_LOGICAL];
+struct adress_logical amountAl[NUMBER_ADRESSES_LOGICAL];
+
+int read_adress(char* name_file){
 
     FILE* file;
-
-    file = fopen("adress.txt", "read");
-
     char c;
+    int i = 0;
 
-    do{
-        c = getc(file);    
-        printf("%c" , c);    
-    }while (c != EOF);
+    file = fopen(name_file, "r");
+
+    if(file == NULL){
+        printf("O arquivo não pode ser aberto\n");
+        return 1;
+    }
+
+    while (!feof(file))
+    {
+        fscanf(file, "%d ", &adresses_logical_numbers[i]);
+        amountAl[i].number_page = adresses_logical_numbers[i] & MASKPAGENUMBER;
+        amountAl[i].offset = adresses_logical_numbers[i] & MASKOFFSET;
+        i++;
+    }
+
+    for(int i = 0; i < NUMBER_ADRESSES_LOGICAL; i++){
+        printf("adreess %d %d\n", amountAl[i].number_page, amountAl[i].offset);
+    }
+
+    return 0;
 
 }
 
-int main(int argc, char const *argv[])
+int main(int argc, char *argv[])
 {
-    
-    read_adress();
+    char *name_file;
+    name_file = argv[1];
+
+    read_adress(name_file);
 
     return 0;
 }
